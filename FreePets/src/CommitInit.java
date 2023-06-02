a934bb9c9c24bbf0605bb425aba47b20567b107c
hotvely <44083916+hotvely@users.noreply.github.com>
2023-06-02 17:23:26 +0900

    test commit

--- FreePets/src/CommitInit.java
+++ FreePets/src/CommitInit.java
@@ -4,6 +4,8 @@ public class CommitInit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("아오 ㅡㅡ ");
+		
+		System.out.println("집에서 커밋 테스트중;");
 	}
 
 }
