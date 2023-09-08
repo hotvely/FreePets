47f9f1c05b508b66bd3e92438866eed958a24946
hotvely <hwan_o3o@naver.com>
2023-09-08 17:09:30 +0900

    2023_09_08 MVC 패키지를 통한 업무 분리
    MVC 패키지 분리 및 업무 할당, Member MVC추가.

--- Freepets/Freepets_backend/README.md
+++ Freepets/Freepets_backend/README.md
@@ -12,4 +12,5 @@
 
 - 9월 8일
 
-1. MVC 구조 코드 작성시작
+1. MVC 구조 코드 작성시작 (폴더 이용해서 업무 분리)
+2. Controller, Service, Repository, domain(vo,Entity) 클래스 생성 및 코드 작성
--- Freepets/Freepets_backend/src/main/java/com/kh/Freepets/domain/member/Member.java
+++ Freepets/Freepets_backend/src/main/java/com/kh/Freepets/domain/member/Member.java
@@ -9,23 +9,57 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.util.Date;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicInsert          //<- orcle에서 default value 사용하기 위함
 public class Member
 {
     @Id
     private String id;
+
     @Column
     private String password;
+
     @Column
     private String name;
+
+    @Column
+    private Date birth;
+
+    @Column
+    private char gender;
+
+    @Column
+    private String phone;
+
     @Column
     private String address;
+
     @Column
-    private String auth;
+    private String nickname;
+
     @Column
-    private int enabled;
+    private String email;
+
+    @Column
+    private Date create_account_date;
+
+    @Column
+    private char delete_account_yn;
+
+    @Column
+    private String authority;
+
+    @Column
+    private String member_img;
+
+    @Column
+    private String member_info;
+
 }
