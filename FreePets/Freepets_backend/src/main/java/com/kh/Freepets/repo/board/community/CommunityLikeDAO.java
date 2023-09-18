4abfa48f79ea2cffdfacf3bc9067edbc2ad15cd4
cookiekingjlim <133620407+cookiekingjlim@users.noreply.github.com>
2023-09-18 15:35:09 +0900

    feat: 폴더 변경 죽여

--- FreePets/Freepets_backend/src/main/java/com/kh/Freepets/repo/board/community/CommunityLikeDAO.java
+++
@@ -1,7 +0,0 @@
-package com.kh.Freepets.repo.board.community;
-
-import com.kh.Freepets.domain.board.community.CommunityLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunityLikeDAO extends JpaRepository<CommunityLike, Integer> {
-}
